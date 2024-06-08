af825c29cd2667a563df45553d527c394f05e4ef
Vallepu Srinivas <159715640+Srinivas1204@users.noreply.github.com>
2024-06-08 20:57:29 +0530

    Update main.js

--- Analog_Clock/main.js
+++ Analog_Clock/main.js
@@ -8,5 +8,5 @@ function clockTime(){
     let date = new Date();
     secHand.style.transform = `rotate(${date.getSeconds()*6}deg)`
     minHand.style.transform = `rotate(${date.getMinutes()*6}deg)`
-    hrHand.style.transform = `rotate(${date.getHours()*30}deg)`
+    hrHand.style.transform = `rotate(${date.getHours()*30 + date.getMinutes()*0.5}deg )`
 }
